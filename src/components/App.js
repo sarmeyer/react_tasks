ebf5e0bf2e7965bccf5e9847fd118d9893d0f491
Sarah <sarahmichellemeyer@gmail.com>
2017-09-12 18:56:25 -0600

    added actions, reducer, and constants for redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
 import '../Main.css';
+import { connect } from 'react-redux';
+import {addReminder} from '../actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="title">Task Reminder</div>
         <div className="form-inline">
           <div className="form-group">
-            <input className="form-Control"
-            placeholder="Enter Task" />
+            <input
+              className="form-Control"
+              placeholder="Enter Task"
+              onChange={event => this.setState({text: event.target.value})}
+            />
           </div>
-          <button type="button" className="btn btn-success">Set Reminder</button>
+          <button type="button" className="btn btn-success" onClick={() => this.addReminder()}>Set Reminder</button>
         </div>
       </div>
     )
   }
 }
 
-export default App;
+export default connect(null, {addReminder}) (App);
